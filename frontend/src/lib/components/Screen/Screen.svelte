96b9a2c0ecdd2423227055f34bcd28b91e7dacea
Isaac_Lagoy <isaacblagoy@gmail.com>
2026-09-18 10:10:06 -0500

    Added screen

---
+++ frontend/src/lib/components/Screen/Screen.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import './screen.css';
+
+	let {
+		children,
+		class: className = ''
+	}: { children: Snippet; class?: string } = $props();
+</script>
+
+<div class={['screen mx-auto', className]}>
+	{@render children()}
+</div>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Button from '$lib/components/Button/Button.svelte';
+	import Screen from '$lib/components/Screen/Screen.svelte';
 
 	function start() {
 		console.log('Start');
@@ -11,7 +12,11 @@
 </script>
 
 <div class="flex justify-center items-center h-screen flex-col gap-0">
-	<Button class="w-96" onclick={start}>
+	<Screen class="w-50 h-50 flex items-center justify-center">
+		<h1 class="text-center text-2xl">Sojourn</h1>
+	</Screen>
+
+	<Button class="w-96 mt-4" onclick={start}>
 		<h1 class="text-center text-2xl">Start</h1>
 	</Button>
 
