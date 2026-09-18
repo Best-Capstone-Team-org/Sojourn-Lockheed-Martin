a31b50bacd25f155df073e0fe843b0dd30599957
Isaac_Lagoy <isaacblagoy@gmail.com>
2026-09-18 10:37:06 -0500

    Simplified screen

---
+++ frontend/src/routes/ground-system/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import Screen from '$lib/components/Screen/Screen.svelte';
+</script>
+
+<div class="flex justify-center items-center h-screen flex-col gap-0">
+	<Screen class="w-50 h-50 flex items-center justify-center">
+		<h1 class="text-center text-2xl">Sojourn</h1>
+	</Screen>
+</div>
+
