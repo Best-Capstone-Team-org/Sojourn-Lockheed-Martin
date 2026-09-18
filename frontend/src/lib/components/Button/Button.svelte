d80883fa13d7e6ae245a9a3d9b58f4d986f25bb8
Isaac_Lagoy <isaacblagoy@gmail.com>
2026-09-18 09:29:41 -0500

    Added buttons and style

---
+++ frontend/src/lib/components/Button/Button.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import type { HTMLButtonAttributes } from 'svelte/elements';
+	import Box from '$lib/components/Box/Box.svelte';
+	import './button.css';
+
+	let {
+		children,
+		class: className = '',
+		type = 'button',
+		...rest
+	}: HTMLButtonAttributes & { children: Snippet } = $props();
+</script>
+
+<button {type} class="standard-button-reset" {...rest}>
+	<Box class={`standard-button ${className}`}>
+		{@render children()}
+	</Box>
+</button>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,34 +1,23 @@
 <script lang="ts">
-	import { apiWebSocket } from "$lib/api";
-	import Box from "$lib/components/Box/Box.svelte";
+	import Button from '$lib/components/Button/Button.svelte';
 
-	const ws = apiWebSocket("command-ws");
-	let ready = $state(false);
-	let resp = $state("");
+	function start() {
+		console.log('Start');
+	}
 
-	ws.onopen = () => {
-		ready = true;
-	};
-	ws.onmessage = (ev) => {
-		resp += `${ev.data}\n`;
-	};
-
-	$effect(() => {
-		if (ready) {
-			ws.send("hello world");
-		}
-	});
+	function openOptions() {
+		console.log('Options');
+	}
 </script>
 
-<Box>
-	<h1 class="text-center text-2xl">Sojourn</h1>
-</Box>
+<div class="flex justify-center items-center h-screen flex-col gap-0">
+	<Button class="w-96" onclick={start}>
+		<h1 class="text-center text-2xl">Start</h1>
+	</Button>
 
-<h1>Welcome to SvelteKit</h1>
-<p>
-	Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
-	documentation
-</p>
+	<Button class="w-96 mt-4" onclick={openOptions}>
+		<h1 class="text-center text-2xl">Options</h1>
+	</Button>
+</div>
 
-<h2>Responses:</h2>
-<p>{resp}</p>
+
