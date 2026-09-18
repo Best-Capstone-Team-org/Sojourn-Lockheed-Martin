0d16fc661856c057e92bbf638936ceae456fa8ab
Isaac_Lagoy <isaacblagoy@gmail.com>
2026-09-18 09:29:41 -0500

    Added buttons and style

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,7 +1,23 @@
 <script lang="ts">
-	import Box from '$lib/components/Box/Box.svelte';
+	import Button from '$lib/components/Button/Button.svelte';
+
+	function start() {
+		console.log('Start');
+	}
+
+	function openOptions() {
+		console.log('Options');
+	}
 </script>
 
-<Box>
-	<h1 class="text-center text-2xl">Sojourn</h1>
-</Box>
+<div class="flex justify-center items-center h-screen flex-col gap-0">
+	<Button class="w-96" onclick={start}>
+		<h1 class="text-center text-2xl">Start</h1>
+	</Button>
+
+	<Button class="w-96 mt-4" onclick={openOptions}>
+		<h1 class="text-center text-2xl">Options</h1>
+	</Button>
+</div>
+
+
