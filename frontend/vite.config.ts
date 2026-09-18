52d5f50c2269342bc2fa999b98b5c812d13daf1a
Owen Shadburne <91618801+CubeTures@users.noreply.github.com>
2026-09-18 15:47:30 -0500

    Fix frontend formatting

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,7 @@
-import tailwindcss from '@tailwindcss/vite';
-import adapter from '@sveltejs/adapter-static';
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import tailwindcss from "@tailwindcss/vite";
+import adapter from "@sveltejs/adapter-static";
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
 	plugins: [
@@ -10,14 +10,16 @@ export default defineConfig({
 			compilerOptions: {
 				// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
 				runes: ({ filename }) =>
-					filename.split(/[/\\]/).includes('node_modules') ? undefined : true
+					filename.split(/[/\\]/).includes("node_modules")
+						? undefined
+						: true,
 			},
-			adapter: adapter()
-		})
+			adapter: adapter(),
+		}),
 	],
 	resolve: process.env.VITEST
 		? {
-				conditions: ['browser']
+				conditions: ["browser"],
 			}
-		: undefined
+		: undefined,
 });
